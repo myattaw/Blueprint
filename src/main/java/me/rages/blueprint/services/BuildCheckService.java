f99182ae2ee95d8741a436d40bde750f681e7b05
Michael <whitefoxpvp@gmail.com>
2025-01-19 14:46:08 -0500

    Added dispenser and tile entity limits for blueprints.

--- src/main/java/me/rages/blueprint/services/BuildCheckService.java
+++ src/main/java/me/rages/blueprint/services/BuildCheckService.java
@@ -5,35 +5,60 @@ import me.rages.blueprint.services.factions.FactionService;
 import me.rages.blueprint.services.skyblock.SkyblockService;
 import me.rages.blueprint.util.Util;
 import me.rages.reliableframework.pluginservice.PluginService;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
+
+import java.util.Arrays;
 
 public class BuildCheckService implements PluginService<BuildCheckService> {
 
     private SkyblockService skyblockService;
     private FactionService factionService;
 
+    private int dispenserLimit;
+    private int tileEntityLimit;
+
+
     @Override
     public BuildCheckService setup(JavaPlugin plugin) {
         if (plugin.getServer().getPluginManager().isPluginEnabled("SuperiorSkyblock2")) {
             this.skyblockService = new SkyblockService().setup(plugin);
         }
-        System.out.println();
         if (plugin.getServer().getPluginManager().isPluginEnabled("Factions")) {
             this.factionService = new FactionService().setup(plugin);
         }
+
+        dispenserLimit = plugin.getConfig().getInt("settings.dispenser-limit");
+        tileEntityLimit = plugin.getConfig().getInt("settings.tile-entity-limit");
+
         return this;
     }
 
     public boolean canBuild(Player player, Points<Vector, Vector> points, Location loc) {
-        if (skyblockService != null) {
-            return skyblockService.canBuild(player, points, loc);
+
+        Chunk chunk = loc.getChunk();
+        BlockState[] tileEntities = chunk.getTileEntities();
+
+        // Check tile entity limit
+        if (tileEntityLimit != -1 && tileEntities.length > tileEntityLimit) {
+            player.sendMessage(ChatColor.RED + String.format("There is more than %d tile entities in this chunk!", tileEntityLimit));
+            return false;
         }
 
-        if (factionService != null) {
-            return factionService.canBuild(player, points, loc);
+        // Count dispensers and enforce dispenser limit
+        long dispenserCount = Arrays.stream(tileEntities)
+                .filter(blockState -> blockState.getType() == Material.DISPENSER)
+                .count();
+
+        if (dispenserLimit != -1 && dispenserCount > dispenserLimit) {
+            player.sendMessage(ChatColor.RED + String.format("There is more than %d dispensers in this chunk!", dispenserLimit));
+            return false;
         }
 
         for (int x : new int[]{points.getMin().getBlockX(), points.getMax().getBlockX()}) {
@@ -43,6 +68,14 @@ public class BuildCheckService implements PluginService<BuildCheckService> {
                     return false;
                 }
             }
+        }
+
+        if (skyblockService != null) {
+            return skyblockService.canBuild(player, points, loc);
+        }
+
+        if (factionService != null) {
+            return factionService.canBuild(player, points, loc);
         }
 
         return true;
